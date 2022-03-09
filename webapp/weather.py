7fa693464f7b886ad9c8ad162fd927780fd5df16
pavel-yas1405 <pavel.iasicov99@gmail.com>
2022-03-09 14:44:41 +0200

    Добавили поддержку базы данных Flask-SQLAlchemy

--- webapp/weather.py
+++ webapp/weather.py
@@ -4,7 +4,7 @@ import requests
 def weather_by_city(city_name):
     weather_url = current_app.config['WEATHER_URL']
     params = {
-        "key": current_app.config["WEATHER_API_KEY"],
+        "key": current_app.config['WEATHER_API_KEY'],
         "q": city_name,
         "format": "json",
         "num_of_days": 1,
